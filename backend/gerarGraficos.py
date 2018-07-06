3bb8a40d09de8530efb1946a85a78268c7e3ecd4
Kelvin Alves <kelvinpalves@gmail.com>
2018-07-06 13:35:40 -0300

    versão em python

---
+++ backend/gerarGraficos.py
@@ -0,0 +1,113 @@
+import numpy
+import matplotlib
+import matplotlib.pyplot as grafico
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-t", "--text", dest="entradaTexto")
+parser.add_argument("-f", "--frequencia", dest="frequencia")
+parser.add_argument("-d", "--debug", dest="modoDebug", default=False)
+
+args = parser.parse_args()
+
+FILE_ASK     = 'tmp/grafico-ask.png'
+FILE_NRZ     = 'tmp/grafico-nrz.png'
+FILE_FFT     = 'tmp/grafico-fft.png'
+FILE_IFFT    = 'tmp/grafico-ifft.png'
+entradaTexto = args.entradaTexto
+frequencia   = float(args.frequencia)
+modoDebug    = args.modoDebug
+
+binarioTexto = ' '.join(bin(ord(x))[2:].zfill(8) for x in entradaTexto)
+
+binarioTextoAuxiliar = binarioTexto.replace(' ', '')
+
+listaBits = []
+for caracter in binarioTextoAuxiliar:
+    listaBits.append(int(caracter))
+    
+if modoDebug:
+    print 'Entrada............:', entradaTexto
+    print 'Frequencia.........:', frequencia
+    print 'Binario Texto......:', binarioTexto
+    print 'Lista Bits.........:', listaBits
+
+listaEixoX = list(numpy.repeat(range(len(listaBits)+1), 2))
+listaEixoX = listaEixoX[1:]
+listaNRZ   = []
+
+for bit in listaBits:
+    listaNRZ.append(1 if bit == 0 else -1)
+
+listaEixoY       = list(numpy.repeat(listaNRZ, 2))
+ultimoValorEixoY = listaEixoY[len(listaEixoY) - 1]
+listaEixoY.append(1 if ultimoValorEixoY else 0)
+
+if modoDebug:
+    print 'Eixo X.............:', listaEixoX
+    print 'Eixo Y.............:', listaEixoY
+    print 'NRZ................:', listaNRZ
+
+imagem = grafico.figure(figsize=(8, 2.5)) 
+
+grafico.title('Codificacao NRZ-L')
+grafico.ylim(-2, 2)
+grafico.plot(listaEixoX, listaEixoY)
+
+imagem.savefig(FILE_NRZ)
+
+for index in range(0, len(listaNRZ)):
+    if (listaNRZ[index] == -1):
+        listaNRZ[index] = 0
+
+dimensao = 16 if len(listaBits) == 8 else len(listaNRZ)
+
+listaASK = []
+
+for bit in listaNRZ:
+    listaASK = numpy.concatenate((listaASK, (numpy.ones(dimensao) * bit)))
+
+dimensaoAuxiliar = len(listaASK)
+listaTempo = numpy.linspace(0, len(listaNRZ), dimensaoAuxiliar)
+
+if modoDebug:
+    print 'Lista Bits - TEMPO: ', listaTempo
+
+sinal = 2 * numpy.pi * frequencia * listaTempo
+listaSinal = numpy.sin(sinal)
+
+if modoDebug:
+    print 'Sinal', listaSinal
+
+sinalFinal = (listaASK * listaSinal)
+
+imagem = grafico.figure(figsize=(8, 2.5)) 
+
+grafico.title('ASK')
+grafico.ylabel('Amplitude')
+grafico.ylim(-2, 2)
+grafico.plot(listaTempo, sinalFinal)
+
+imagem.savefig(FILE_ASK)
+
+fft             = numpy.fft.fft(sinalFinal)
+dimensaoSinal   = len(sinalFinal)
+auxiliar        = dimensaoSinal / dimensao
+listaFrequencia = numpy.arange(dimensaoSinal) / auxiliar
+listaFFT        = fft / dimensaoSinal
+
+imagem = grafico.figure(figsize=(8, 2.5)) 
+
+grafico.title('Dominio da Frequencia')
+grafico.plot(listaFrequencia,listaFFT.real)
+
+imagem.savefig(FILE_FFT)
+
+demodulacao = numpy.fft.ifft(fft)
+
+imagem = grafico.figure(figsize=(8, 2.5)) 
+
+grafico.plot(listaTempo, demodulacao.real)
+grafico.title('Demodulacao')
+
+imagem.savefig(FILE_IFFT)
